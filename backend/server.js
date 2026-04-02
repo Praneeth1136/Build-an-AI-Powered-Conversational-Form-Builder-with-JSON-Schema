2670326143cccb72ead617f842fcf6ac75a883f6
PRANEETH <23a91a05g3@aec.edu.in>
2026-04-02 20:04:31 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.API_PORT || 8080;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); 
 
 // Healthcheck Endpoint
 app.get('/health', (req, res) => {
